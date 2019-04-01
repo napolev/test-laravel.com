76b8aa436e818124bb076a6fca80e3fedc9ea41f
napolev <>
2019-03-31 22:22:12 -0500

    Added route: '/echo-post' as follows:
    Route::post('/echo-post', function (Request $request) {
        return request()->all();
    });
    
    This can be checked with Postman with the following request URL:
    
    https://www.test-laravel-direct.com/api/echo-post?param1=value1&param2=value2&param3=value3
    
    Virtual Host config:
    
    DocumentRoot "${INSTALL_DIR}/www/[WEBSITE-DIR]/api/public"

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/echo-post', function (Request $request) {
+    return request()->all();
+});
